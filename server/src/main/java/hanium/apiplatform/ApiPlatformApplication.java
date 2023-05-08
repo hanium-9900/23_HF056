068aa8588af1067799456d30ffd624a3b28a4422
Seungheon Lee <rumah800@naver.com>
2023-05-08 18:51:04 +0900

    스프링 기본 세팅

---
+++ server/src/main/java/hanium/apiplatform/ApiPlatformApplication.java
@@ -0,0 +1,13 @@
+package hanium.apiplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPlatformApplication.class, args);
+	}
+
+}
---
+++ server/src/main/java/hanium/apiplatform/controller/TestController.java
@@ -0,0 +1,13 @@
+package hanium.apiplatform.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("test")
+    public String test() {
+        return "test success";
+    }
+}
