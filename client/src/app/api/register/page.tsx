911c71561c1cf9db033cda288d031ba7dd7cdf36
Chanhyuk Byeon <bdu00chch@gmail.com>
2023-05-15 20:56:55 +0900

    api register 페이지 기본 동작 구현 (TODO: module)

--- client/src/app/api/register/page.tsx
+++ client/src/app/api/register/page.tsx
@@ -2,18 +2,15 @@
 
 import { useEffect, useState } from 'react';
 import './page.css';
-import { headers } from 'next/dist/client/components/headers';
-import { error } from 'console';
 
 interface ServiceInfo {
-  name: string;
+  title: string;
   description: string;
   price: number;
   key: string;
 }
 
 interface ApiInfo {
-  id: number;
   host: string;
   method: string;
   path: string;
@@ -39,120 +36,116 @@ interface ApiInfo {
   }[];
 }
 
-let lastApiId = 0;
-
 export default function Register() {
   const [serviceInfo, setServiceInfo] = useState<ServiceInfo>({
-    name: '',
+    title: '',
     description: '',
     price: -1,
     key: '',
   });
   const [apiList, setApiList] = useState<ApiInfo[]>([]);
 
-  const submitForm = (event: { preventDefault: () => void }) => {
+  const submitForm = async (event: { preventDefault: () => void }) => {
     event.preventDefault();
 
+    // [TODO] 서버쪽 required 가 int라서 bool -> int 매핑 과정이 필요.
+    // 추후 서버쪽 required 타입 변경 검토.
     const data = {
       ...serviceInfo,
-      apis: apiList,
+      apis: apiList.map(api => ({
+        ...api,
+        headers: api.headers.map(header => ({
+          ...header,
+          required: 1,
+        })),
+        parameters: api.parameters.map(parameter => ({
+          ...parameter,
+          required: 1,
+        })),
+        responseParameters: api.responseParameters.map(responseParameter => ({
+          ...responseParameter,
+          required: 1,
+        })),
+      })),
     };
 
-    console.log(data);
+    try {
+      await fetch('http://localhost:8080/api', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      console.log(data);
+      alert(JSON.stringify(data, undefined, 2));
+    } catch (e) {
+      console.error(e);
+      alert(e);
+    }
   };
 
-  const addHeader = (apiId: number) => {
-    console.log('add header', apiId);
+  const addHeader = (apiIdx: number) => {
+    console.log('add header', apiIdx);
 
     //
-    const keys = apiList.find(api => api.id === apiId)?.headers.map(header => header.key);
-    if (!keys || keys.includes('')) {
+    const keys = apiList[apiIdx].headers.map(header => header.key);
+    if (keys.includes('')) {
       alert('먼저 헤더명을 모두 입력해주세요');
       return;
     }
 
-    const updatedApiList = [...apiList].map(api => {
-      if (api.id === apiId) {
-        return {
-          ...api,
-          headers: [...api.headers, { key: '', description: '', required: false }],
-        };
-      } else {
-        return api;
-      }
-    });
+    const updatedApiList = [...apiList];
+    updatedApiList[apiIdx].headers.push({ key: '', description: '', required: false });
 
     setApiList(updatedApiList);
   };
 
-  const addParameter = (apiId: number) => {
-    console.log('add parameter', apiId);
+  const addParameter = (apiIdx: number) => {
+    console.log('add parameter', apiIdx);
 
     //
-    const keys = apiList.find(api => api.id === apiId)?.parameters.map(parameter => parameter.key);
-    if (!keys || keys.includes('')) {
+    const keys = apiList[apiIdx].parameters.map(parameter => parameter.key);
+    if (keys.includes('')) {
       alert('먼저 파라미터명을 모두 입력해주세요');
       return;
     }
 
-    const updatedApiList = [...apiList].map(api => {
-      if (api.id === apiId) {
-        return {
-          ...api,
-          parameters: [...api.parameters, { key: '', description: '', required: false }],
-        };
-      } else {
-        return api;
-      }
-    });
+    const updatedApiList = [...apiList];
+    updatedApiList[apiIdx].parameters.push({ key: '', description: '', required: false });
 
     setApiList(updatedApiList);
   };
 
-  const addResponseParameter = (apiId: number) => {
-    console.log('add response parameter', apiId);
+  const addResponseParameter = (apiIdx: number) => {
+    console.log('add response parameter', apiIdx);
 
     //
-    const keys = apiList.find(api => api.id === apiId)?.responseParameters.map(responseParameter => responseParameter.key);
-    if (!keys || keys.includes('')) {
+    const keys = apiList[apiIdx].responseParameters.map(responseParameter => responseParameter.key);
+    if (keys.includes('')) {
       alert('먼저 응답 파라미터명을 모두 입력해주세요');
       return;
     }
 
-    const updatedApiList = [...apiList].map(api => {
-      if (api.id === apiId) {
-        return {
-          ...api,
-          responseParameters: [...api.responseParameters, { key: '', description: '', required: false }],
-        };
-      } else {
-        return api;
-      }
-    });
+    const updatedApiList = [...apiList];
+    updatedApiList[apiIdx].responseParameters.push({ key: '', description: '', required: false });
 
     setApiList(updatedApiList);
   };
 
-  const addErrorCode = (apiId: number) => {
-    console.log('add error code', apiId);
+  const addErrorCode = (apiIdx: number) => {
+    console.log('add error code', apiIdx);
 
     //
-    const keys = apiList.find(api => api.id === apiId)?.errorCodes.map(errorCode => errorCode.key);
-    if (!keys || keys.includes('')) {
+    const keys = apiList[apiIdx].errorCodes.map(errorCode => errorCode.key);
+    if (keys.includes('')) {
       alert('먼저 에러 코드룰 모두 입력해주세요');
       return;
     }
 
-    const updatedApiList = [...apiList].map(api => {
-      if (api.id === apiId) {
-        return {
-          ...api,
-          errorCodes: [...api.errorCodes, { key: '', description: '' }],
-        };
-      } else {
-        return api;
-      }
-    });
+    const updatedApiList = [...apiList];
+    updatedApiList[apiIdx].errorCodes.push({ key: '', description: '' });
 
     setApiList(updatedApiList);
   };
@@ -164,7 +157,6 @@ export default function Register() {
     setApiList([
       ...apiList,
       {
-        id: ++lastApiId,
         host: '',
         method: 'GET',
         path: '',
@@ -177,20 +169,12 @@ export default function Register() {
     ]);
   };
 
-  const updateApiInfo = (apiId: number, info: Partial<ApiInfo>) => {
-    console.log('update api info', apiId, info);
+  const updateApiInfo = (apiIdx: number, info: Partial<ApiInfo>) => {
+    console.log('update api info', apiIdx, info);
 
     //
-    const updatedApiList = [...apiList].map(api => {
-      if (api.id === apiId) {
-        return {
-          ...api,
-          ...info,
-        };
-      } else {
-        return api;
-      }
-    });
+    const updatedApiList = [...apiList];
+    updatedApiList[apiIdx] = { ...updatedApiList[apiIdx], ...info };
 
     setApiList(updatedApiList);
   };
@@ -206,7 +190,7 @@ export default function Register() {
         {/* 서비스 명세 */}
         <label className="block mb-6">
           <div className="font-bold mb-2">서비스 이름</div>
-          <input type="text" placeholder="서비스 이름을 입력하세요" onChange={e => setServiceInfo({ ...serviceInfo, name: e.target.value })} />
+          <input type="text" placeholder="서비스 이름을 입력하세요" onChange={e => setServiceInfo({ ...serviceInfo, title: e.target.value })} />
         </label>
         <label className="block mb-6">
           <div className="font-bold mb-2">서비스 설명</div>
@@ -230,48 +214,71 @@ export default function Register() {
           <hr className="mb-12" />
 
           {/* API 목록 */}
-          {apiList.map((api, idx) => (
-            <div key={api.id} className="">
+          {apiList.map((api, apiIdx) => (
+            <div key={apiIdx} className="">
               {/* API Host */}
               <label className="flex items-center mb-6">
-                <span className="rounded bg-blue-500 text-white shrink-0 py-2 px-4 mr-5">{idx + 1}번</span>
+                <span className="rounded bg-blue-500 text-white shrink-0 py-2 px-4 mr-5">{apiIdx + 1}번</span>
                 <div className="flex items-center grow">
                   <div className="font-bold mr-3">Host</div>
-                  <input type="text" placeholder="https://example.com" onChange={e => updateApiInfo(api.id, { host: e.target.value })} />
+                  <input type="text" placeholder="https://example.com" onChange={e => updateApiInfo(apiIdx, { host: e.target.value })} />
                 </div>
               </label>
               {/* API Endpoint */}
               <div className="flex items-center mb-6">
-                <select className="mr-3" onChange={e => updateApiInfo(api.id, { method: e.target.value })}>
+                <select className="mr-3" onChange={e => updateApiInfo(apiIdx, { method: e.target.value })}>
                   <option value="GET">GET</option>
                   <option value="POST">POST</option>
                   <option value="PUT">PUT</option>
                   <option value="PATCH">PATCH</option>
                   <option value="DELETE">DELETE</option>
                 </select>
-                <input type="text" placeholder="/example/api/path" onChange={e => updateApiInfo(api.id, { path: e.target.value })} />
+                <input type="text" placeholder="/example/api/path" onChange={e => updateApiInfo(apiIdx, { path: e.target.value })} />
               </div>
               {/* Description */}
               <label className="block mb-6">
                 <div className="font-bold mb-2">API 설명</div>
-                <textarea rows={5} placeholder="API 설명을 입력하세요" onChange={e => updateApiInfo(api.id, { description: e.target.value })}></textarea>
+                <textarea rows={5} placeholder="API 설명을 입력하세요" onChange={e => updateApiInfo(apiIdx, { description: e.target.value })}></textarea>
               </label>
               {/* Headers */}
               <label className="block mb-6">
                 <div className="font-bold mb-2">헤더</div>
                 <div>
                   {api.headers.map((header, idx) => (
-                    <div key={`idx-${header.key}`} className="flex mb-4">
-                      <input className="mr-3" type="text" placeholder="헤더명" />
-                      <input type="text" placeholder="설명" />
+                    <div key={idx} className="flex mb-4">
+                      <input
+                        className="mr-3"
+                        type="text"
+                        placeholder="헤더명"
+                        onChange={e => {
+                          const updatedHeaders = [...apiList[apiIdx].headers];
+                          updatedHeaders[idx] = { ...updatedHeaders[idx], key: e.target.value };
+
+                          updateApiInfo(apiIdx, {
+                            headers: updatedHeaders,
+                          });
+                        }}
+                      />
+                      <input
+                        type="text"
+                        placeholder="설명"
+                        onChange={e => {
+                          const updatedHeaders = [...apiList[apiIdx].headers];
+                          updatedHeaders[idx] = { ...updatedHeaders[idx], description: e.target.value };
+
+                          updateApiInfo(apiIdx, {
+                            headers: updatedHeaders,
+                          });
+                        }}
+                      />
                     </div>
                   ))}
                   <div className="flex justify-end">
                     <button
                       className="rounded border-2 border-blue-500 bg-white-500 text-blue-500 py-3 px-6 transition-colors hover:bg-blue-500 hover:text-white"
-                      onClick={() => addHeader(api.id)}
+                      onClick={() => addHeader(apiIdx)}
                     >
-                      헤더 추가 (미구현)
+                      헤더 추가
                     </button>
                   </div>
                 </div>
@@ -281,17 +288,40 @@ export default function Register() {
                 <div className="font-bold mb-2">요청 파라미터</div>
                 <div>
                   {api.parameters.map((parameter, idx) => (
-                    <div key={`idx-${parameter.key}`} className="flex mb-4">
-                      <input className="mr-3" type="text" placeholder="변수명" />
-                      <input type="text" placeholder="설명" />
+                    <div key={idx} className="flex mb-4">
+                      <input
+                        className="mr-3"
+                        type="text"
+                        placeholder="변수명"
+                        onChange={e => {
+                          const updatedParameters = [...apiList[apiIdx].parameters];
+                          updatedParameters[idx] = { ...updatedParameters[idx], key: e.target.value };
+
+                          updateApiInfo(apiIdx, {
+                            parameters: updatedParameters,
+                          });
+                        }}
+                      />
+                      <input
+                        type="text"
+                        placeholder="설명"
+                        onChange={e => {
+                          const updatedParameters = [...apiList[apiIdx].parameters];
+                          updatedParameters[idx] = { ...updatedParameters[idx], description: e.target.value };
+
+                          updateApiInfo(apiIdx, {
+                            parameters: updatedParameters,
+                          });
+                        }}
+                      />
                     </div>
                   ))}
                   <div className="flex justify-end">
                     <button
                       className="rounded border-2 border-blue-500 bg-white-500 text-blue-500 py-3 px-6 transition-colors hover:bg-blue-500 hover:text-white"
-                      onClick={() => addParameter(api.id)}
+                      onClick={() => addParameter(apiIdx)}
                     >
-                      파라미터 추가 (미구현)
+                      파라미터 추가
                     </button>
                   </div>
                 </div>
@@ -301,17 +331,40 @@ export default function Register() {
                 <div className="font-bold mb-2">응답 파라미터</div>
                 <div>
                   {api.responseParameters.map((responseParameters, idx) => (
-                    <div key={`idx-${responseParameters.key}`} className="flex mb-4">
-                      <input className="mr-3" type="text" placeholder="변수명" />
-                      <input type="text" placeholder="설명" />
+                    <div key={idx} className="flex mb-4">
+                      <input
+                        className="mr-3"
+                        type="text"
+                        placeholder="변수명"
+                        onChange={e => {
+                          const updatedResponseParameters = [...apiList[apiIdx].responseParameters];
+                          updatedResponseParameters[idx] = { ...updatedResponseParameters[idx], key: e.target.value };
+
+                          updateApiInfo(apiIdx, {
+                            responseParameters: updatedResponseParameters,
+                          });
+                        }}
+                      />
+                      <input
+                        type="text"
+                        placeholder="설명"
+                        onChange={e => {
+                          const updatedResponseParameters = [...apiList[apiIdx].responseParameters];
+                          updatedResponseParameters[idx] = { ...updatedResponseParameters[idx], description: e.target.value };
+
+                          updateApiInfo(apiIdx, {
+                            responseParameters: updatedResponseParameters,
+                          });
+                        }}
+                      />
                     </div>
                   ))}
                   <div className="flex justify-end">
                     <button
                       className="rounded border-2 border-blue-500 bg-white-500 text-blue-500 py-3 px-6 transition-colors hover:bg-blue-500 hover:text-white"
-                      onClick={() => addResponseParameter(api.id)}
+                      onClick={() => addResponseParameter(apiIdx)}
                     >
-                      파라미터 추가 (미구현)
+                      파라미터 추가
                     </button>
                   </div>
                 </div>
@@ -321,17 +374,40 @@ export default function Register() {
                 <div className="font-bold mb-2">에러 코드</div>
                 <div>
                   {api.errorCodes.map((errorCode, idx) => (
-                    <div key={`idx-${errorCode.key}`} className="flex mb-4">
-                      <input className="mr-3" type="text" placeholder="에러 코드" />
-                      <input type="text" placeholder="설명" />
+                    <div key={idx} className="flex mb-4">
+                      <input
+                        className="mr-3"
+                        type="text"
+                        placeholder="에러 코드"
+                        onChange={e => {
+                          const updatedErrorCodes = [...apiList[apiIdx].errorCodes];
+                          updatedErrorCodes[idx] = { ...updatedErrorCodes[idx], key: e.target.value };
+
+                          updateApiInfo(apiIdx, {
+                            errorCodes: updatedErrorCodes,
+                          });
+                        }}
+                      />
+                      <input
+                        type="text"
+                        placeholder="설명"
+                        onChange={e => {
+                          const updatedErrorCodes = [...apiList[apiIdx].errorCodes];
+                          updatedErrorCodes[idx] = { ...updatedErrorCodes[idx], description: e.target.value };
+
+                          updateApiInfo(apiIdx, {
+                            errorCodes: updatedErrorCodes,
+                          });
+                        }}
+                      />
                     </div>
                   ))}
                   <div className="flex justify-end">
                     <button
                       className="rounded border-2 border-blue-500 bg-white-500 text-blue-500 py-3 px-6 transition-colors hover:bg-blue-500 hover:text-white"
-                      onClick={() => addErrorCode(api.id)}
+                      onClick={() => addErrorCode(apiIdx)}
                     >
-                      에러 코드 추가 (미구현)
+                      에러 코드 추가
                     </button>
                   </div>
                 </div>
@@ -351,7 +427,7 @@ export default function Register() {
 
         <div className="flex justify-end mb-12">
           <button className="rounded bg-blue-500 text-white py-3 px-6" onClick={addApi}>
-            API 추가 (미구현)
+            API 추가
           </button>
         </div>
 
