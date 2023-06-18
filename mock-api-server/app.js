a4ba922fff8602a1918d671aa29149762f6ff026
seungheonL <rumah800@naver.com>
2023-06-18 16:11:09 +0900

    제공데이터 수정 및 키 검증 추가

--- mock-api-server/app.js
+++ mock-api-server/app.js
@@ -5,41 +5,48 @@ const app = express();
 const bigData = [
   {
     id: 1,
-    name: '데이터1',
-    value: 10,
-    category: '카테고리1',
-    description: '이것은 데이터1입니다.',
+    name: 'iPhone 12',
+    price: 999,
+    category: 'Electronics',
+    description: 'The latest iPhone model.',
     timestamp: '2023-06-18 10:00:00',
     location: {
       latitude: 37.12345,
       longitude: 127.6789,
     },
-    tags: ['태그1', '태그2'],
+    tags: ['Apple', 'Smartphone'],
     metadata: {
-      source: '데이터 출처',
-      owner: '데이터 소유자',
+      source: 'Apple Store',
+      owner: 'Apple Inc.',
     },
   },
   {
     id: 2,
-    name: '데이터2',
-    value: 20,
-    category: '카테고리2',
-    description: '이것은 데이터2입니다.',
+    name: 'Samsung Galaxy S21',
+    price: 899,
+    category: 'Electronics',
+    description: 'A flagship smartphone from Samsung.',
     timestamp: '2023-06-18 11:00:00',
     location: {
       latitude: 37.54321,
       longitude: 127.09876,
     },
-    tags: ['태그3', '태그4'],
+    tags: ['Samsung', 'Smartphone'],
     metadata: {
-      source: '데이터 출처',
-      owner: '데이터 소유자',
+      source: 'Samsung Store',
+      owner: 'Samsung Electronics',
     },
   },
 ];
 
 app.get('/bigdata', (req, res) => {
+  const key = req.query.key;
+
+  if (key != '12345') {
+    res.status(400).json({ message: 'API Key is Not Valid' });
+    return;
+  }
+
   res.json(bigData);
 });
 
