49d543eaad95a345a2ac6538a0c9454979e2cdf5
seungheonL <rumah800@naver.com>
2023-06-23 12:39:41 +0900

    모크 api 날씨 데이터로 변경

--- mock-api-server/app.js
+++ mock-api-server/app.js
@@ -2,37 +2,43 @@ import express from 'express';
 
 const app = express();
 
-const bigData = [
-  {
-    name: 'iPhone 12',
-    price: 999000,
-    description: 'The latest iPhone model.',
-    owner: 'Apple Inc.',
-  },
-  {
-    name: 'Samsung Galaxy S21',
-    price: 899000,
-    description: 'A flagship smartphone from Samsung.',
-    owner: 'Samsung Electronics',
-  },
-];
+const weatherData = {
+  date: '2023-06-23',
+  time: '15:00',
+  temperature: 25,
+  humidity: 60,
+  condition: '맑음',
+};
 
-app.get('/phone', (req, res) => {
+app.get('/weather', (req, res) => {
   const key = req.header('X-API-KEY');
-  const { corporation } = req.query;
+  let { daysAgo } = req.query;
+
+  daysAgo = parseInt(daysAgo);
+
+  const currentDate = new Date();
+  currentDate.setDate(currentDate.getDate() - daysAgo);
+  const targetDate = currentDate.toISOString().split('T')[0];
+
+  weatherData.date = targetDate;
 
   if (key != '12345') {
     res.status(401).json({ message: 'API Key is Not Valid' });
     return;
   }
 
-  if (corporation == 'apple') {
-    res.json(bigData[0]);
+  if (!Number.isInteger(daysAgo)) {
+    res.status(427).json({ message: '<daysAgo> must be integer' });
     return;
   }
 
-  if (corporation == 'samsung') {
-    res.json(bigData[1]);
+  if (daysAgo < 0) {
+    res.status(430).json({ message: '<daysAgo> must be positive' });
+    return;
+  }
+
+  if (daysAgo >= 0) {
+    res.json(weatherData);
     return;
   }
 
