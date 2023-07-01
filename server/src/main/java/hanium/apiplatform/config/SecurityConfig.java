cb085506168165b80aaec386ec1420627e4353ac
seungheonL <rumah800@naver.com>
2023-07-01 23:35:07 +0900

    origin IP 주소 변경

--- server/src/main/java/hanium/apiplatform/config/SecurityConfig.java
+++ server/src/main/java/hanium/apiplatform/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedOrigin("http://3.34.215.14:3000/");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
