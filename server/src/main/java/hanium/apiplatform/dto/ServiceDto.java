42c164f84e0d41cde0cfa6bbbfb134b0ce122572
seungheonL <rumah800@naver.com>
2023-08-01 00:39:21 +0900

    Service 조회 시 User 정보 포함하도록 변경

--- server/src/main/java/hanium/apiplatform/dto/ServiceDto.java
+++ server/src/main/java/hanium/apiplatform/dto/ServiceDto.java
@@ -3,8 +3,6 @@ package hanium.apiplatform.dto;
 import hanium.apiplatform.entity.Api;
 import hanium.apiplatform.entity.Service;
 import java.util.ArrayList;
-
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,6 +18,7 @@ public class ServiceDto {
     private int price;
     private String key;
     private ArrayList<ApiDto> apis = new ArrayList<>();
+    private UserDto user;
 
     public static ServiceDto toDto(Service service) {
         ServiceDto serviceDto = new ServiceDto();
@@ -28,6 +27,7 @@ public class ServiceDto {
         serviceDto.setDescription(service.getDescription());
         serviceDto.setPrice(service.getPrice());
         serviceDto.setKey(service.getKey());
+        serviceDto.setUser(UserDto.toDto(service.getUser()));
 
         for (Api api : service.getApis()) {
             serviceDto.apis.add(ApiDto.toDto(api));
