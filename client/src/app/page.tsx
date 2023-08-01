9787d28b27bab473d64d79fef297fc739b903a9f
Chanhyuk Byeon <bdu00chch@gmail.com>
2023-08-01 21:14:11 +0900

    feat: 서비스 목록 페이지 링크 추가

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -6,7 +6,8 @@ export default function Home() {
       <div className="text-xl mb-10">메인페이지(미구현)</div>
       <div className="flex flex-col gap-3 text-blue-500 ">
         <Link href="/services/register">- 서비스 등록 페이지</Link>
-        <Link href="/services/1">- 서비스 조회 페이지(id=1)</Link>
+        <Link href="/services">- 서비스 목록 페이지</Link>
+        <Link href="/services/1">- 서비스 조회 페이지(id=1, dummy)</Link>
       </div>
     </main>
   );
