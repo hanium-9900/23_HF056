c687980d4ea8664646db8034c93ccfea1ab3b322
Chanhyuk Byeon <bdu00chch@gmail.com>
2023-08-01 20:18:14 +0900

    fix: axios interceptor 서버사이드 문제 해결(분기 추가)

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,12 +1,19 @@
 import axios from 'axios';
+import { Session, getServerSession } from 'next-auth';
 import { getSession } from 'next-auth/react';
+import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions';
 
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_BASEURL,
 });
 
 instance.interceptors.request.use(async request => {
-  const session = await getSession();
+  let session: Session | null = null;
+  if (typeof window === 'undefined') {
+    session = await getServerSession(authOptions);
+  } else {
+    session = await getSession();
+  }
 
   console.log('calling?', session);
 
