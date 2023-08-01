19b82c6f02dc5893e9c3d617aee1150b474055d9
Chanhyuk Byeon <bdu00chch@gmail.com>
2023-08-01 15:18:56 +0900

    feat: API 추가(더미)

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,13 +1,31 @@
 import axios from './axios';
 
 export const api = {
+  /**
+   * 회원관리
+   */
   auth: {
+    /**
+     * 회원가입
+     *
+     * @param credentials 회원가입 정보
+     */
     register(credentials: { email: string; password: string }) {
       return axios.post('/users/join', credentials);
     },
+    /**
+     * 로그인
+     *
+     * @param credentials 로그인 정보
+     */
     login(credentials: { email: string; password: string }) {
       return axios.post<string>('/users/login', credentials);
     },
+    /**
+     * 사용자 정보 가져오기
+     *
+     * @param token 토큰 (로그인 과정에만 필요)
+     */
     me(token?: string) {
       if (token) {
         return axios.get<{ id: string; email: string }>('/users/me', {
@@ -20,9 +38,67 @@ export const api = {
       }
     },
   },
+  /**
+   * 서비스
+   */
   services: {
+    /**
+     * 서비스 등록
+     */
     register(service: any) {
       return axios.post('/services', service);
+    },
+    /**
+     * 서비스 수정
+     */
+    update(service: any) {
+      // 수정
+    },
+    /**
+     * 서비스 목록 조회
+     */
+    list() {
+      return axios.get('/services');
+    },
+    /**
+     * 서비스 상세 조회
+     */
+    show(serviceId: number) {
+      return axios.get(`/services?id=${serviceId}`);
+    },
+    /**
+     * 서비스 신고
+     */
+    claim(serviceId: number, content: string) {
+      // 신고
+    },
+  },
+  /**
+   * 사용량
+   */
+  limitations: {
+    /**
+     * 사용량 조회
+     */
+    show(serviceId: number) {
+      // 조회
+    },
+    /**
+     * 사용량 제한 설정
+     */
+    update(serviceId: number, limitation: any) {
+      // 수정/설정
+    },
+  },
+  /**
+   * 에러 로그
+   */
+  errorLogs: {
+    /**
+     * 에러 로그 목록 조회
+     */
+    list(serviceId: number) {
+      // 목록 조회
     },
   },
 };
