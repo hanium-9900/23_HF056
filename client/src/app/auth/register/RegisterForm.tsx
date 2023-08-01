8e02a6ed2ccb1263ca339fa4d98e98963e429e30
Chanhyuk Byeon <bdu00chch@gmail.com>
2023-08-01 21:50:40 +0900

    fix: 회원가입 이후 redirect & loading state

--- client/src/app/auth/register/RegisterForm.tsx
+++ client/src/app/auth/register/RegisterForm.tsx
@@ -1,17 +1,24 @@
 'use client';
 
 import Link from 'next/link';
-import { FormEvent, useRef } from 'react';
+import { FormEvent, useRef, useState } from 'react';
 import axios from 'axios';
 import { api } from '@/api';
+import { useRouter } from 'next/navigation';
 
 export default function RegisterForm() {
+  const router = useRouter();
+
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
   const passwordConfirmRef = useRef<HTMLInputElement>(null);
 
+  const [loading, setLoading] = useState(false);
+
   async function submit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
+
+    if (loading) return;
 
     const email = emailRef.current?.value;
     const password = passwordRef.current?.value;
@@ -32,9 +39,11 @@ export default function RegisterForm() {
     };
 
     try {
-      const { data } = await api.auth.register(credentials);
+      setLoading(() => true);
+      await api.auth.register(credentials);
 
-      alert(data);
+      alert('회원가입이 완료되었습니다!');
+      router.replace('/auth/login');
     } catch (e) {
       if (axios.isAxiosError<{ message: string }>(e)) {
         if (e.response?.data.message === 'Duplicate Email') {
@@ -43,38 +52,37 @@ export default function RegisterForm() {
           alert('알 수 없는 오류가 발생했습니다!');
         }
       }
+    } finally {
+      setLoading(() => false);
     }
   }
 
   return (
-    <main className="max-w-md mx-auto py-14">
-      <h1 className="text-3xl font-bold text-center mb-7">회원가입</h1>
-      <form onSubmit={submit}>
-        <div className="mb-4">
-          <label htmlFor="email" className="inline-block font-bold mb-2">
-            이메일
-          </label>
-          <input ref={emailRef} id="email" type="email" name="email" required />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="password" className="inline-block font-bold mb-2">
-            비밀번호
-          </label>
-          <input ref={passwordRef} id="password" type="password" name="password" required />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="password_confirm" className="inline-block font-bold mb-2">
-            비밀번호 확인
-          </label>
-          <input ref={passwordConfirmRef} id="password_confirm" type="password" name="password_confirm" required />
-        </div>
-        <div className="flex justify-end">
-          <Link href="/auth/login" className="btn btn-form-outline mr-3">
-            로그인
-          </Link>
-          <button className="btn btn-form">회원가입</button>
-        </div>
-      </form>
-    </main>
+    <form onSubmit={submit}>
+      <div className="mb-4">
+        <label htmlFor="email" className="inline-block font-bold mb-2">
+          이메일
+        </label>
+        <input ref={emailRef} id="email" type="email" name="email" required />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="password" className="inline-block font-bold mb-2">
+          비밀번호
+        </label>
+        <input ref={passwordRef} id="password" type="password" name="password" required />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="password_confirm" className="inline-block font-bold mb-2">
+          비밀번호 확인
+        </label>
+        <input ref={passwordConfirmRef} id="password_confirm" type="password" name="password_confirm" required />
+      </div>
+      <div className="flex justify-end">
+        <Link href="/auth/login" className="btn btn-form-outline mr-3">
+          로그인
+        </Link>
+        <button className="btn btn-form">회원가입</button>
+      </div>
+    </form>
   );
 }
