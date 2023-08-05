4df087f25a40ec607f1aac80ab042fdc6428ea21
Chanhyuk Byeon <bdu00chch@gmail.com>
2023-08-05 15:59:56 +0900

    fix: server restart script ./nohup.out 2

--- server/restart.sh
+++ server/restart.sh
@@ -2,6 +2,6 @@
 
 sudo kill -9 `sudo lsof -t -i:8080`
 
-(cd /home/ubuntu/projects/23_HF056/server/build/libs && ./nohup.out java -jar api-platform-0.0.1-SNAPSHOT.jar &)
+(cd /home/ubuntu/projects/23_HF056/server/build/libs && nohup java -jar api-platform-0.0.1-SNAPSHOT.jar &)
 
 disown
