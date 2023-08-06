4d6eba3bdc6dfeb7038363ea7ea4469fcf27193c
Chanhyuk Byeon <bdu00chch@gmail.com>
2023-08-07 02:22:06 +0900

    fix: 수정 페이지 타 서비스 protect

--- client/src/app/services/[serviceId]/edit/page.tsx
+++ client/src/app/services/[serviceId]/edit/page.tsx
@@ -1,9 +1,21 @@
 import { api } from '@/api';
 // import './page.css';
 import ServiceRegisterForm from '@/app/services/register/components/ServiceRegisterForm';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 
 export default async function Register({ params }: { params: { serviceId: string } }) {
+  const serviceId = Number.parseInt(params.serviceId);
+  if (Number.isNaN(serviceId)) {
+    redirect('/');
+  }
+
   const { data: service } = await api.services.show(Number.parseInt(params.serviceId));
+
+  const session = await getServerSession();
+  if (session?.user?.email !== service.user.email) {
+    redirect(`/services/${params.serviceId}`)
+  }
 
   return (
     <main className="max-w-3xl mx-auto py-14">
