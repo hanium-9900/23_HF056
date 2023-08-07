5dc01b85a18966ba53b84fd6828e2dddedf5d5f8
sminha <sminha8590@naver.com>
2023-08-07 11:02:19 +0900

    feat: 서버에서 서비스 정보 가져오기

--- client/src/app/services/[serviceId]/page.tsx
+++ client/src/app/services/[serviceId]/page.tsx
@@ -7,55 +7,72 @@ import ApiSpecification from './components/ApiSpecification';
 import ApiPurchaseButton from './components/ApiPurchaseButton';
 import Link from 'next/link';
 
+import { api, ServiceResponse } from '@/api';
+
 export default function ServiceInfoPage({ params }: { params: { serviceId: string } }) {
   // [TEMP] 임시 데이터
-  const [service, setService] = useState<Service>({
-    category: '기타',
-    title: '대구광역시 행정동별 유동인구',
-    price: 30000000,
-    description:
-      '1500만 사용자의 통신 데이터를 근간으로 대구광역시 지역단위(광역시도, 시군구, 읍면동, 50M구역)별 유동인구 정보로 시간대별, 성별, 연령별로 유동인구 정보를 확인 할 수 있는 상품데이터',
-    key: 'asd123asd123asd123',
-    apis: [
-      {
-        host: 'https://example.com',
-        method: 'GET',
-        description: '테스트 API 1번',
-        path: '/example/api1',
-        limitation: 1000,
-        headers: [
-          {
-            key: '테스트 헤더1',
-            description: '테스트 헤더1 설명',
-            required: true,
-          },
-        ],
-        requestParameters: [],
-        responseParameters: [],
-        errorCodes: [],
-      },
-      {
-        host: 'https://example.com',
-        method: 'GET',
-        description: '테스트 API 2번',
-        path: '/example/api2',
-        limitation: 2000,
-        headers: [
-          {
-            key: '테스트 헤더2',
-            description: '테스트 헤더2 설명',
-            required: false,
-          },
-        ],
-        requestParameters: [],
-        responseParameters: [],
-        errorCodes: [],
-      },
-    ],
-  });
+  // const [service, setService] = useState<Service>({
+  //   category: '기타',
+  //   title: '대구광역시 행정동별 유동인구',
+  //   price: 30000000,
+  //   description:
+  //     '1500만 사용자의 통신 데이터를 근간으로 대구광역시 지역단위(광역시도, 시군구, 읍면동, 50M구역)별 유동인구 정보로 시간대별, 성별, 연령별로 유동인구 정보를 확인 할 수 있는 상품데이터',
+  //   key: 'asd123asd123asd123',
+  //   apis: [
+  //     {
+  //       host: 'https://example.com',
+  //       method: 'GET',
+  //       description: '테스트 API 1번',
+  //       path: '/example/api1',
+  //       limitation: 1000,
+  //       headers: [
+  //         {
+  //           key: '테스트 헤더1',
+  //           description: '테스트 헤더1 설명',
+  //           required: true,
+  //         },
+  //       ],
+  //       requestParameters: [],
+  //       responseParameters: [],
+  //       errorCodes: [],
+  //     },
+  //     {
+  //       host: 'https://example.com',
+  //       method: 'GET',
+  //       description: '테스트 API 2번',
+  //       path: '/example/api2',
+  //       limitation: 2000,
+  //       headers: [
+  //         {
+  //           key: '테스트 헤더2',
+  //           description: '테스트 헤더2 설명',
+  //           required: false,
+  //         },
+  //       ],
+  //       requestParameters: [],
+  //       responseParameters: [],
+  //       errorCodes: [],
+  //     },
+  //   ],
+  // });
+
+  const [service, setService] = useState<Service | null>(null);
   const [selectedApi, setSelectedApi] = useState(0);
 
   // [TODO] 서버로부터 가져오기
+  useEffect(() => {
+    api.services.show(Number(params.serviceId))
+      .then(response => {
+        setService(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, [params.serviceId]);
+
+  if (!service) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <main className="container xl:max-w-5xl mx-auto py-10 px-3">
