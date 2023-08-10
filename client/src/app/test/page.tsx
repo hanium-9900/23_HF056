d975e00c4334e6882370ed65fc1906ad84ed55c9
Chanhyuk Byeon <bdu00chch@gmail.com>
2023-08-10 17:09:04 +0900

    dev: JsonSchemaEditor 데모 테스트 페이지

---
+++ client/src/app/test/page.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { useState } from "react";
+import JsonSchemaEditor from "../components/JsonSchemaEditor";
+
+export default function Page() {
+  const [schema, setSchema] = useState<any>(null)
+
+  return (
+    <div className="max-w-3xl mx-auto">
+      <div className="mb-6">
+        <JsonSchemaEditor onChange={(e) => {
+          const schema = JSON.parse(e)
+          setSchema(schema)
+        }} />
+      </div>
+      <div>
+        <div className="mb-2 text-xl font-bold">결과 Schema</div>
+        <pre className="p-2 rounded bg-gray-200">
+          <code>{JSON.stringify(schema, undefined, 2)}</code>
+        </pre>
+      </div>
+    </div >
+  )
+}
