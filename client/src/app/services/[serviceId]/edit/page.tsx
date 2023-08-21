e9d6692cad5fef2da478f05db4ef4f85e8fe11d8
Chanhyuk Byeon <bdu00chch@gmail.com>
2023-08-21 20:38:06 +0900

    feat: 수정 페이지 상단에 원본 글 링크 추가

--- client/src/app/services/[serviceId]/edit/page.tsx
+++ client/src/app/services/[serviceId]/edit/page.tsx
@@ -2,6 +2,7 @@ import { api } from '@/api';
 // import './page.css';
 import ServiceRegisterForm from '@/app/services/register/components/ServiceRegisterForm';
 import { getServerSession } from 'next-auth';
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
 export default async function Register({ params }: { params: { serviceId: string } }) {
@@ -19,6 +20,9 @@ export default async function Register({ params }: { params: { serviceId: string
 
   return (
     <main className="max-w-3xl mx-auto py-14">
+      <div className='text-center mb-3'>
+        <Link className='text-blue-500 text-lg font-bold text-center' href={`/services/${service.id}`}>{service.title}</Link>
+      </div>
       <h1 className="text-3xl font-bold text-center mb-7">서비스 수정</h1>
       <ServiceRegisterForm service={service} />
     </main>
